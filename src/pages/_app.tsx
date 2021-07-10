ffba5a0297eb2d7241075fd8d89f5423751466fb
JoaoVSouto <joaovitorss99@hotmail.com>
2021-07-09 23:14:09 -0300

    Add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
+import GlobalStyles from 'styles/global';
 import light from 'styles/themes/light';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={light}>
       <Component {...pageProps} />
+
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
