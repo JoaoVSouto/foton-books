9093045c81b67a565c1ad2c21eb1720344e7611e
JoaoVSouto <joaovitorss99@hotmail.com>
2021-07-11 23:30:04 -0300

    Configure container breakpoints

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,8 @@
+import { generateMedia } from 'styled-media-query';
+
+export default generateMedia({
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+});
--- src/styles/mixins/container.ts
+++ src/styles/mixins/container.ts
@@ -1,6 +1,26 @@
 import { css } from 'styled-components';
 
+import breakpoints from 'styles/breakpoints';
+
 export default css`
   padding-inline-start: 1.25rem;
   padding-inline-end: 1.25rem;
+  margin-left: auto;
+  margin-right: auto;
+
+  ${breakpoints.greaterThan('sm')`
+    max-width: 540px;
+  `}
+
+  ${breakpoints.greaterThan('md')`
+    max-width: 720px;
+  `}
+
+  ${breakpoints.greaterThan('lg')`
+    max-width: 960px;
+  `}
+
+  ${breakpoints.greaterThan('xl')`
+    max-width: 1140px;
+  `}
 `;
