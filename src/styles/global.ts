dd104c383df44a5095448e666ddc53b1cc71b06b
JoaoVSouto <joaovitorss99@hotmail.com>
2021-07-11 23:46:59 -0300

    Add page font increasing

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 import bookSliderStyles from 'components/BookSlider/styles';
 
 import fontsConfiguration from './fonts';
+import breakpoints from './breakpoints';
 
 export default createGlobalStyle`
   ${fontsConfiguration}
@@ -13,6 +14,20 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    ${breakpoints.greaterThan('md')`
+      font-size: 18px;
+    `}
+
+    ${breakpoints.greaterThan('lg')`
+      font-size: 20px;
+    `}
+
+    ${breakpoints.greaterThan('xl')`
+      font-size: 22px;
+    `}
   }
 
   body {
